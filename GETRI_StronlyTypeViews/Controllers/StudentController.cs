833a6650163927131b3bf9ed06f3dbd298335954
Pratik <pratik.guvnl@gmail.com>
2023-12-18 10:50:40 +0530

    Developed Student Class

---
+++ GETRI_StronlyTypeViews/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using GETRI_StronlyTypeViews.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GETRI_StronlyTypeViews.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student() { StudentId="ST-101", StudentName="Student1", StudentBranch="IT", StudentSection="Programming", StudentGender="Male"},
+                new Student() { StudentId="ST-501", StudentName="Student2", StudentBranch="EC", StudentSection="MicroPorcessor", StudentGender="FeMale"},
+                new Student() { StudentId="ST-701", StudentName="Student3", StudentBranch="Electrical", StudentSection="Battery", StudentGender="Male"},
+                new Student() { StudentId="ST-901", StudentName="Student4", StudentBranch="Mechanical", StudentSection="Pumps", StudentGender="FeMale"}
+            };
+
+            return View(students);
+        }
+    }
+}
---
+++ GETRI_StronlyTypeViews/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace GETRI_StronlyTypeViews.Models
+{
+    public class Student
+    {
+        public string StudentId { get; set; }
+        public string StudentName { get; set;}
+        public string StudentBranch { get; set;}
+        public string StudentSection { get; set;}
+        public string StudentGender { get; set;}
+    }
+}
