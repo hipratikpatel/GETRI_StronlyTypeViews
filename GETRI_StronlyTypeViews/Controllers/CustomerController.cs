a9a4047f378f9715f65bad7b61662ee1348069f8
Pratik <pratik.guvnl@gmail.com>
2023-12-18 10:37:54 +0530

    Customer Class Done

--- GETRI_StronlyTypeViews/Controllers/CustomerController.cs
+++ GETRI_StronlyTypeViews/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GETRI_StronlyTypeViews.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GETRI_StronlyTypeViews.Controllers
 {
@@ -6,7 +7,26 @@ namespace GETRI_StronlyTypeViews.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<Customer> lstCustomer = new List<Customer>
+            {
+                new Customer
+                {
+                    CustomerId=1,CustomerName="Tejas",CustomerAddress="Rajkot",CustomerPhone="123456"
+                },
+                new Customer
+                {
+                    CustomerId=2,CustomerName="Saurabh",CustomerAddress="Vidhyut Nagar",CustomerPhone="654321"
+                },
+                new Customer
+                {
+                    CustomerId=3,CustomerName="Amit",CustomerAddress="Kalali",CustomerPhone="456789"
+                },
+                new Customer
+                {
+                    CustomerId=4,CustomerName="Pratik",CustomerAddress="Gotri",CustomerPhone="987654"
+                }
+            };
+            return View(lstCustomer);
         }
     }
 }
